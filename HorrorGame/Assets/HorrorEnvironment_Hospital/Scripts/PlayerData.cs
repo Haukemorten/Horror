25b15c295d1db2836f08b578aae0f181db24da2f
Haukemorten <107413393+Haukemorten@users.noreply.github.com>
2022-07-21 19:14:43 +0200

    ++

--- HorrorGame/Assets/HorrorEnvironment_Hospital/Scripts/PlayerData.cs
+++ HorrorGame/Assets/HorrorEnvironment_Hospital/Scripts/PlayerData.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class PlayerData : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+   private float [] posiion;
 
-    // Update is called once per frame
-    void Update()
+   private int lvl;
+   
+
+
+    public PlayerData(PlayerData player)
     {
-        
+     
+
+    
+    
     }
 }
